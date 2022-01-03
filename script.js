0ad44986c9ae89f5698d30474afe8ab720f6aa84
Topaz Jos <topazjos@Topaz.local>
2022-01-03 19:50:47 +0200

    Remove book on  click

--- script.js
+++ script.js
@@ -20,4 +20,15 @@ class ObjectBook {
       <button class="rmBtn">Remove</button>
       </article>`;
     });
+
+  Container.innerHTML = books;
+  this.removeButton = document.querySelectorAll('button.rmBtn');
+  this.removeButton.forEach((btn, index) => {
+  btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  this.removeBook(index);
+  });
+  });
+  }
+  
   
